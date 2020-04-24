2d8dfb60516ba76a00839c8969a378ee88abf66e
jamesnitz <jamesanitz2@gmail.com>
2020-04-24 15:38:34 -0500

    Create products is fully functioning

---
+++ Bangazon/Controllers/ProductsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bangazon.Data;
+using Bangazon.Models;
+using Bangazon.Models.ProductViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bangazon.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProductsController(ApplicationDbContext context, UserManager<ApplicationUser> usermanager)
+        {
+            _userManager = usermanager;
+            _context = context;
+        }
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public async Task<ActionResult> Create()
+        {
+            var productTypes = await _context.ProductType
+                .Select(p => new SelectListItem() { Text = p.Label, Value = p.ProductTypeId.ToString()})
+                .ToListAsync();
+            var viewmodel = new ProductFormViewModel();
+            viewmodel.ProductTypeOptions = productTypes;
+            return View(viewmodel);
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProductFormViewModel productViewModel)
+        {
+            try
+            {
+                var user = await GetCurrentUserAsync();
+
+                var product = new Product
+                {
+                    DateCreated = productViewModel.DateCreated,
+                    Price = productViewModel.Price,
+                    Title = productViewModel.Title,
+                    Description = productViewModel.Description,
+                    UserId = user.Id,
+                    Quantity = productViewModel.Quantity,
+                    ProductTypeId = productViewModel.ProductTypeId
+                };
+
+                _context.Product.Add(product);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private async Task<ApplicationUser> GetCurrentUserAsync() => await _userManager.GetUserAsync(HttpContext.User);
+
+    }
+}
---
+++ Bangazon/Models/ProductViewModels/ProductFormViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductFormViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public string City { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public bool Active { get; set; }
+        public int ProductTypeId { get; set; }
+        public List<SelectListItem> ProductTypeOptions { get; set; }
+
+    }
+}
