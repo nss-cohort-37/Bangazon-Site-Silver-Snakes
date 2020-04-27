ad303310fa31af25baecd0833dd9b5c77ca1862f
Kevin Penny <Kevin.penny3@yahoo.com>
2020-04-27 10:36:52 -0500

    add new payment working and populating in the payment type list

---
+++ Bangazon/Controllers/PaymentTypesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bangazon.Data;
+using Bangazon.Models;
+using Bangazon.Models.PaymentTypeModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bangazon.Controllers
+{
+    public class PaymentTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PaymentTypesController(ApplicationDbContext context, UserManager<ApplicationUser> usermanager)
+        {
+            _userManager = usermanager;
+            _context = context;
+        }
+        // GET: PaymentTypes
+        public async Task<ActionResult> Index()
+        {
+            var user = await GetCurrentUserAsync();
+            var paymentTypes = _context.PaymentType
+                .Where(pt => pt.UserId == user.Id);
+            return View(paymentTypes);
+        }
+
+        // GET: PaymentTypes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PaymentTypes/Create
+        public ActionResult Create()
+        {
+            var viewModel = new PaymentTypeCreateViewModel();
+            return View(viewModel);
+        }
+
+        // POST: PaymentTypes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(PaymentTypeCreateViewModel PaymentTypeCreateViewModel)
+        {
+            try
+            {
+                var paymentType = new PaymentType
+                {
+                    Description = PaymentTypeCreateViewModel.Description,
+                    AccountNumber = PaymentTypeCreateViewModel.AccountNumber
+                };
+                var user = await GetCurrentUserAsync();
+                paymentType.UserId = user.Id;
+
+                _context.PaymentType.Add(paymentType);
+                await _context.SaveChangesAsync();
+
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PaymentTypes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentTypes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PaymentTypes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentTypes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+    }
+}
---
+++ Bangazon/Models/PaymentTypeModels/PaymentTypeCreateViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.PaymentTypeModels
+{
+    public class PaymentTypeCreateViewModel
+    {
+        [Key]
+        public int PaymentTypeId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(55)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+    }
+}
