b091ffc1cc7741cd9b0083eddaae5ed46e2d322c
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-04-29 14:09:56 -0400

    added link to view order histroy on profile

---
+++ Bangazon/Models/ProfileViewModels/ProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon.Models.ProfileViewModels
+{
+    public class ProfileViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+    }
+}
